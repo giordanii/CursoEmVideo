71fe55ebeafe3c5dba2caa23b427c5fb320ffac8
João Giordani <jgiordanii@gmail.com>
2022-11-28 09:37:52 -0300

    commit

--- js/aula11/script.js
+++ js/aula11/script.js
@@ -1,11 +1,11 @@
-function speedometer
+function speedometer() {
 var vel = document.getElementById('speed')
 var res = document.querySelector('div#res')
 var vel1 = Number(vel.value)
 
-res.innerHTML = `<p>Your current speed is <strong>${vel}</strong></p>`
-if (vel > 60) {
-    res.innerHTML += `<p>You will receive a ticket! You are driving faster than you should.</p>`
-} else {
-    res.innerHTML += `<p>Never drink and drive! Always wear seatbelt.</p>
+res.innerHTML = `<p>Your current speed is <strong>${vel1}</strong> km/h.</p>`
+if (vel1 > 60) {
+    res.innerHTML += `<p>You <strong>RECEIVED</strong> a ticket! You are driving faster than you should.</p>`
+}
+    res.innerHTML += `<p>Never drink and drive! Always wear seatbelt.</p>`
 }
