0f72d804231519cf61a9fa589eeafd5a3bd294c6
João Giordani <jgiordanii@gmail.com>
2022-12-06 22:48:22 -0300

    final commit

--- js/ex1/assets/script.js
+++ js/ex1/assets/script.js
@@ -1,18 +1,21 @@
 function load() {
     var msg = window.document.getElementById('msg');
-    var img = window.document.getElementById('image');
     var data = new Date();
-    //var hour = data.getHours();
-    var hour = 18
+    var hour = data.getHours();
+    var h1 = document.getElementById('title');
     msg.innerHTML = `Now it is ${hour} hours.`
     if (hour >= 0 && hour < 12) {
         //GOOD MORNING
-        img.innerHTML = '<img src="./assets/morning.jpg">'
+        photo.innerHTML = '<img src="./assets/morning.jpg" alt = "time of the day">'
+        document.body.style.background = 'rgb(255, 186, 124)'
     } else if (hour >= 12 && hour < 18) {
         //GOOD AFTERNOON
-        img.innerHTML = '<img src = "./assets/afternoon.jpg">'
+        photo.innerHTML = '<img src = "./assets/afternoon.jpg" alt = "time of the day">'
+        document.body.style.background = 'rgb(1, 123, 192)'
     } else {
         //GOOD NIGHT
-        img.innerHTML = '<img src = "./assets/night.jpg">'
+        photo.innerHTML = '<img src = "./assets/night.jpg" alt = "time of the day">'
+        document.body.style.background = 'rgb(25, 58, 57)'
+        h1.style.color = 'white'
     }
 }
