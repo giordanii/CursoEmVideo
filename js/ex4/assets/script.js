8a080d31793ac70ebdbe3d11416fc99e2b5c2b65
João Giordani <jgiordanii@gmail.com>
2022-12-11 19:46:09 -0300

    commit

---
+++ js/ex4/assets/script.js
@@ -0,0 +1,18 @@
+function math(){
+    var num = document.getElementById('nb1')
+    var tab = document.getElementById('seltab')
+    var i = Number(nb1.value)
+
+    if(num.value.length == ''){
+        window.alert('Please enter a number.')
+    } else {
+        let c = 1
+        tab.innerHTML = ''
+        while(c <= 10){
+            let item = document.createElement('option');
+            item.text = `${i} x ${c} = ${i*c}`
+            tab.appendChild(item)
+            c++
+        }  
+    }
+}
